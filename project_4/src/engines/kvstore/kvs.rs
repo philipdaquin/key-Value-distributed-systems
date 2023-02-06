bd8e1046bf929dbb751f8224cb8d53bf4ac9db14
philipdaquin <philipdaquin20@gmail.com>
2023-02-07 06:42:45 +0800

    separated kvstore, kvread, kvwrite

--- project_4/src/engines/kvstore/kvs.rs
+++ project_4/src/engines/kvstore/kvs.rs
@@ -4,62 +4,51 @@ use std::{
     process::exit, 
     io::{Write, Seek, Read}, 
     fs::File, 
-    ffi::OsStr
+    ffi::OsStr, sync::{Arc, atomic::AtomicU64}, cell::RefCell
 };
+use parking_lot::{Mutex, RwLock};
+
 use crate::engines::kvstore::{
-    writer::LogWriterWithPos, 
-    reader::LogReaderWithPos, 
+    log_writer::LogWriterWithPos, 
+    log_reader::LogReaderWithPos, 
     command::{CmdMetadata, Command}
 };
 use crate::error::{Result, CacheError};
-use std::fs;
+
 use crate::engines::KvsEngine;
+
+
+use super::{
+    super::{sorted_generation, load_log_file, log_path}, kv_reader::{KvReader, Reader}, kv_writer::{KvWriter, Writer}
+};
 
 const MEM_THRESHOLD: u64 = 1024 * 1024;
 
 pub trait Cache  {
-    fn get(&mut self, key: String) -> Result<Option<String>>;
-    fn set(&mut self, key: String, value: String) -> Result<()>;
+    fn get(&self, key: String) -> Result<Option<String>>;
+    fn set(&self, key: String, value: String) -> Result<()>;
     fn open(path: impl Into<PathBuf>) -> Result<Self> where Self: Sized;
-    fn remove(&mut self, key: String) -> Result<()>; 
+    fn remove(&self, key: String) -> Result<()>; 
     fn version();
-    fn compact(&mut self) -> Result<()>;
-    fn new_log_file(&mut self, gen: u64) -> Result<LogWriterWithPos<File>>;
 }
 
-// #[derive(Serialize, Deserialize, Clone, Default)]
+#[derive(Clone)]
 pub struct KvStore { 
-    
     /// `PathBuf`
     ///  Represents a file or directory on the file system and provides method for with paths
-    directory: PathBuf,
-
-    /// Locates position of inode of file
-    /// 
-    /// Current generation number is a monotononicaly increasing integer that is 
-    /// assigned to each data file when it is created 
-    /// -   It is use to version control the data file and it is incremented each time the data file 
-    ///     is updated or rewritten 
-    /// 
-    /// -   Older version is the lower generation number, which is then considered state   
-    curr_gen: u64,
-
+    directory: Arc<PathBuf>,
     /// In memory key indexes to on disk values 
     /// 
     /// - `Command`: request or the representation of a request made to the database 
     /// - A `BTreeMap` in memory stores the keys and the value locations for fast query 
-    index : BTreeMap<String, CmdMetadata>,
-
+    index : Arc<BTreeMap<String, CmdMetadata>>,
     /// in memory Buffer Writer and writes data individually  
-    writer: LogWriterWithPos<File>,
-
+    writer: Arc<Mutex<KvWriter>>,
     /// In memory Buffer Reader and reads data chunks 
     /// Key: Generational Number, Value: Reader 
-    reader: HashMap<u64, LogReaderWithPos<File>>,
-
-    uncompacted_space: u64
-
+    reader: KvReader,
 }
+
 
 
 impl KvStore { 
@@ -68,10 +57,11 @@ impl KvStore {
     /// The directory and all files in it must be readable by this process
     ///  
     pub fn open(path: impl Into<PathBuf>) -> Result<Self> where Self: Sized {
-        let path = path.into();
+        let path = Arc::new(path.into());
 
         // In Memory Buffer Reader and Buffer Writer 
-        let (mut reader, mut index) = (HashMap::new(), BTreeMap::new());
+        let (mut reader, 
+            mut index) = (HashMap::new(), BTreeMap::new());
         // Current generational value 
         let curr_gen = *sorted_generation(&path)?.last().unwrap_or(&0) + 1;
         
@@ -90,82 +80,35 @@ impl KvStore {
             reader.insert(version, log_reader);
         }
 
-        Ok(Self { 
-            directory: path, 
-            index, 
-            reader, 
-            curr_gen,  
+      
+        let kv_read = KvReader {
+            reader: RwLock::new(reader),
+            index: index.clone().into(),
+            directory: Arc::clone(&path), 
+            safe_point: Arc::new(AtomicU64::new(0))
+        };
+        let kv_writer = KvWriter {
+            
+            directory: Arc::clone(&path), 
+            curr_gen,
             writer,
-            uncompacted_space
+            uncompacted_space,
+            index: Arc::new(RwLock::new(index.clone())),
+            reader: kv_read.clone()
+            
+        };
+
+
+        Ok(Self { 
+            directory: Arc::new(path.to_path_buf()), 
+            index: Arc::new(index), 
+            reader: kv_read, 
+            writer: Arc::new(Mutex::new(kv_writer)),
         })
     }
     pub fn version() {
         println!("{}", env!("CARGO_PKG_VERSION"))
     }
-
-    ///
-    /// Compaction:
-    ///     - Is the process of reducing the size of the database by remove stale commands
-    ///     from the log 
-    fn compact(&mut self) -> Result<()> { 
-        // Create a new generation number for the compacted log file 
-        let compact_gen = self.curr_gen + 1;
-        self.curr_gen += 2;
-
-        // Create a new log file for the current generation 
-        self.writer = self.new_log_file(self.curr_gen)?;
-
-        // Keep track of the new position in the compacted log 
-        let mut new = 0;
-
-        // Create a new log file for the compacted generation 
-        let mut compact_writer = self.new_log_file(compact_gen)?;
-
-        for cmd in &mut self.index.values_mut() { 
-
-            // Check if the log reader exists 
-            if let Some(reader) = self.reader.get_mut(&cmd.generation_num) { 
-                if reader.index != cmd.position { 
-                    reader.seek(std::io::SeekFrom::Start(cmd.position))?;
-                }
-                let mut entry_reader = reader.take(cmd.len);
-                
-                let len = std::io::copy(&mut entry_reader, &mut compact_writer)?;
-                
-                // Update the entry 
-                *cmd = (compact_gen, new..new + len).into();
-
-                // increment the position 
-                new += len
-            }
-        }
-        compact_writer.flush()?;
-
-        // Collect stale generation numbers
-        let stale_gens = self
-            .reader
-            .keys()
-            .filter(|gen| gen < &&compact_gen)
-            .cloned()
-            .collect::<Vec<u64>>();
-
-        // Collect stale generation numbers 
-        for stale in stale_gens { 
-            self.reader.remove(&stale);
-            std::fs::remove_file(log_path(&self.directory, stale))?;
-
-        }
-        // reset the stale files 
-        self.uncompacted_space = 0;
-
-        Ok(())
-        
-    }
-
-    fn new_log_file(&mut self, gen: u64) -> Result<LogWriterWithPos<File>> {
-        LogWriterWithPos::<File>::new_log_file(&self.directory, gen, &mut self.reader)
-    }
-
 }
 
 
@@ -183,24 +126,24 @@ impl KvsEngine for KvStore {
     /// 
     /// 5. The result of the data set is returned to the client
     /// 
-    fn get(&mut self, key: String) -> Result<Option<String>> {
+    fn get(&self, key: String) -> Result<Option<String>> {
         
         // Check if the metadata is in memory 
-        if let Some(CmdMetadata { generation_num , position, len } ) = self.index.get(&key) { 
+        if let Some(cmd) = self.index.get(&key) { 
             // Access the latest version 
-            if let Some(buffer_file) = self.reader.get_mut(generation_num) { 
-                buffer_file.seek(std::io::SeekFrom::Start(*position))?;
-                
-                let command = buffer_file.take(*len);
+            // if let Some(buffer_file) = self.reader.reader.into_inner().get_mut(&cmd.generation_num) { 
+                // buffer_file.seek(std::io::SeekFrom::Start(*position))?;
+                let cmd = cmd.clone();
+                // let command = buffer_file.take(*len);
 
-                if let Command::Set(_, value) = serde_json::from_reader(command)? { 
+                if let Command::Set(_, value) = self.reader.read_command(cmd)? { 
                     return Ok(Some(value))
                 } else { 
                     Err(CacheError::KeyNotFound)
                 }
-            } else { 
-                Err(CacheError::KeyNotFound)
-            }
+            // } else { 
+                // Err(CacheError::KeyNotFound)
+            // }
         } else { 
             Err(CacheError::KeyNotFound)
         }
@@ -219,24 +162,8 @@ impl KvsEngine for KvStore {
     /// 
     /// 4. Set CommandPos in BTreeMap
     /// 
-    fn set(&mut self, key: String, value: String) -> Result<()> {
-        // Represent the command
-        let start = self.writer.index;
-        // Serialise the command to I/O 
-        serde_json::to_writer(&mut self.writer, &Command::Set(key.clone(), value)).expect("");
-
-        // Buffer forced to write to disk immediately
-        self.writer.flush()?;
-
-        // Insert into the in memory key and value positions of command
-        self.index.insert(key, (self.curr_gen, start..self.writer.index).into());
-
-
-        if self.uncompacted_space > MEM_THRESHOLD { 
-            self.compact()?;
-        }
-
-        Ok(())
+    fn set(&self, key: String, value: String) -> Result<()> {
+        self.writer.lock().set(key, value)
 
     }
     ///
@@ -251,90 +178,7 @@ impl KvsEngine for KvStore {
     /// 
     /// 3. WE update our original in memory cache 
     /// 
-    fn remove(&mut self, key: String) -> Result<()> {
-        
-        if self.index.contains_key(&key) { 
-
-            let command = Command::Remove(key);
-
-            // Serialise the command to I/O 
-            serde_json::to_writer(&mut self.writer, &command).expect("");
-
-            // Buffer forced to write to disk immediately
-            self.writer.flush()?;
-
-            if let Command::Remove(val) = command { 
-                self.index.remove(&val).expect("Key not Found!");
-            }
-            Ok(())
-        } else { 
-            Err(CacheError::KeyNotFound.into())
-        }
+    fn remove(&self, key: String) -> Result<()> {
+        self.writer.lock().remove(key)
     }
-
-    
-
 }
-
-///
-/// A generation number is a ID that is used to 
-/// distinguish different versions of a file or data 
-/// 
-fn sorted_generation(path: &Path) -> Result<Vec<u64>> { 
-    let mut gen_list: Vec<u64> = fs::read_dir(&path)?
-        .flat_map(|res| -> Result<_> { Ok(res?.path()) })
-        .filter(|path| path.is_file() && path.extension() == Some("log".as_ref()))
-        .flat_map(|path| {
-            path.file_name()
-                .and_then(OsStr::to_str)
-                .map(|s| s.trim_end_matches(".log"))
-                .map(str::parse::<u64>)
-        })
-        .flatten()
-        .collect();
-    gen_list.sort_unstable();
-    Ok(gen_list)
-}
-
-fn log_path(direction: &Path, gen: u64) -> PathBuf { 
-    direction.join(format!("{}.log", gen))
-}
-
-/// 
-/// Load the whole log file and store value locations in the index map 
-/// 
-/// Return how many bytes can be saved after a compaction 
-fn load_log_file(
-    generation: u64,  
-    reader: &mut LogReaderWithPos<File>, 
-    index: &mut BTreeMap<String, CmdMetadata>
-) -> Result<u64> { 
-
-    let mut start = reader.seek(std::io::SeekFrom::Start(0))?;
-    let mut stream = serde_json::Deserializer::from_reader(reader).into_iter::<Command>();
-    let mut uncompacted_data = 0;
-
-    while let Some(val) = stream.next() { 
-        // 
-        let new = stream.byte_offset() as u64;
-
-        match val? { 
-
-            Command::Set(key, _) => { 
-                if let Some(command) = index.insert(key, (generation, start..new).into()) { 
-                    uncompacted_data += command.len
-                }
-            }
-            
-            Command::Remove(key) => { 
-                if let Some(command) = index.remove(&key) { 
-                    uncompacted_data += command.len;
-                }
-                uncompacted_data += new - start;
-            }
-            _ => {}
-        }
-        start = new
-    }
-    Ok(uncompacted_data)
-} 
