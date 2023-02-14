7a112c66b93ce26f752639a61ebda8070db0e93b
philipdaquin <philipdaquin20@gmail.com>
2023-02-14 19:03:08 +0800

    temp fixed error

---
+++ project_4/benches/benchmark.rs
@@ -0,0 +1,5 @@
+use criterion::criterion_main;
+use criterion::{criterion_group, BatchSize, Criterion};
+
+criterion_group!(benches);
+criterion_main!(benches);
--- project_4/benches/my_bench.rs
+++
@@ -1,5 +0,0 @@
-use criterion::criterion_main;
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion };
-
-
-criterion_main!(benches);
